d292007b05d7335f7db4e80eb37e22eedb2981e8
holdenprkr <holdenprkr@gmail.com>
2020-03-06 10:50:14 -0600

    third commit

--- Cohort.cs
+++ Cohort.cs
@@ -7,7 +7,7 @@ namespace StudentExercises
         public string Name { get; set; }
 
         // public List<Student> Students = new List<Student>();
-        public List<Instructor> Instructors = new List<Instructor>();
+        // public List<Instructor> Instructors = new List<Instructor>();
 
         public Cohort(string name)
         {
